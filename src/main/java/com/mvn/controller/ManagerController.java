8a2e56bed9640f31a6019b9da003e76ec82b42b5
869874873 <869874873@qq.com>
2018-03-26 17:01:41 +0800

    1

--- src/main/java/com/mvn/controller/ManagerController.java
+++ src/main/java/com/mvn/controller/ManagerController.java
@@ -10,16 +10,28 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import com.mvn.model.Manager;
 import com.mvn.service.ManagerService;
 
+/**
+ *
+ * @author cz
+ * @date 2018年3月25日 上午9:35:43
+ *
+ *
+ */
 @Controller
 @RequestMapping("/ctrl/manager")
 public class ManagerController {
-	
+
 	@Autowired
 	private ManagerService managerService;
-	
+
+	/**
+	 * 查询所有管理员信息
+	 * 
+	 * @return
+	 */
 	@ResponseBody
 	@RequestMapping("/selectAllManager")
-	public List<Manager> selectAllManager(){
+	public List<Manager> selectAllManager() {
 		List<Manager> selectAllManager = managerService.selectAllManager();
 		return selectAllManager;
 	}
